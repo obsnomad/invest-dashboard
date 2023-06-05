b2f504812ff94ae8350b192677764c318a00e109
Aleksandr Katelnikov <akatelnikov@yandex.ru>
2023-06-05 20:40:47 +0300

    Update DB source

--- prisma/migrations/20230605173415_initial/migration.sql
+++ prisma/migrations/20230605173415_initial/migration.sql
@@ -1,20 +1,24 @@
 -- CreateTable
 CREATE TABLE "Operation" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
     "type" INTEGER NOT NULL,
     "figi" TEXT NOT NULL,
     "payment" INTEGER NOT NULL,
     "commission" INTEGER NOT NULL,
-    "quantity" INTEGER NOT NULL
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "Operation_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Intrument" (
-    "figi" TEXT NOT NULL PRIMARY KEY,
+    "figi" TEXT NOT NULL,
     "ticker" TEXT NOT NULL,
     "lot" INTEGER NOT NULL,
     "currency" TEXT NOT NULL,
-    "name" TEXT NOT NULL
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Intrument_pkey" PRIMARY KEY ("figi")
 );
