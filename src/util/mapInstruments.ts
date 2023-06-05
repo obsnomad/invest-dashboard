55c44a182c2cdc3d45eeb9ddadfc995b23e0e91d
Aleksandr Katelnikov <akatelnikov@yandex.ru>
2023-06-05 19:51:57 +0300

    Fix types

--- src/util/mapInstruments.ts
+++ src/util/mapInstruments.ts
@@ -1,9 +1,6 @@
-import { Data as InstrumentData, Params as InstrumentParams } from '@/app/api/instrument/[figi]/route';
-import fetch, { Config } from '@/util/fetch';
+import { Instrument } from 'tinkoff-invest-api/cjs/generated/instruments';
 
-const FETCH_PARAMS: Config = {
-    next: { revalidate: 60 },
-};
+import fetch from '@/util/fetch';
 
 export const mapInstruments = async <T extends { figi: string }>(items: T[]) => {
     const instruments = (
@@ -16,22 +13,16 @@ export const mapInstruments = async <T extends { figi: string }>(items: T[]) => 
                     return acc;
                 }, new Set() as Set<string>),
             ].map((figi) =>
-                fetch<InstrumentData, InstrumentParams>(
-                    `${process.env.NEXT_PUBLIC_API_HOST}/api/instrument`,
-                    {
-                        ...FETCH_PARAMS,
-                        params: { figi },
-                    },
-                ),
+                fetch<Instrument>(`${process.env.NEXT_PUBLIC_API_HOST}/api/instrument/${figi}/`),
             ),
         )
     ).reduce((acc, result) => {
         if (result.status === 'fulfilled') {
-            const instrument = result.value as InstrumentData;
+            const instrument = result.value as Instrument;
             acc.set(instrument.figi, instrument);
         }
         return acc;
-    }, new Map() as Map<string, InstrumentData>);
+    }, new Map() as Map<string, Instrument>);
 
     return items.map((item) => ({
         ...item,
